b65d4888ce23bd07d9c9ae591949198fb7b50593
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-11-17 23:20:55 +0200

    added basic CSS

---
+++ src/components/varsCss.js
@@ -0,0 +1,14 @@
+const varsCss = {
+  primaryColor: '#FFFFFF',
+  secondaryColor: '#F7F7F7',
+  accentColor: '#FF6B01',
+  textColor: '#000000',
+  textSecondary: '#545454',
+  textAccentColor: '#FF6B08',
+  textErrorColor: '#FF001B',
+  fontSizePrimary: '12px',
+  fontSizeSecondary: '14px',
+  transition: '300ms',
+};
+
+export default varsCss;
