bfb79a9221da170825a25dd137caaa7a1ad3d33f
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-11-19 00:37:44 +0200

    fixed <MovieItem/>

--- src/components/mainSection/movieItem/movieItem.jsx
+++ src/components/mainSection/movieItem/movieItem.jsx
@@ -4,29 +4,26 @@ import { Movie, Poster, Title, Info, Release } from './movieItem.styled';
 const base_url = 'https://image.tmdb.org/t/p';
 const file_size = '/w300';
 
-function MovieItem({ movies }) {
+function MovieItem({ movie }) {
   return (
     <>
-      {movies &&
-        movies.map(movie => {
-          return (
-            <Movie key={movie.id}>
-              <Poster
-                src={
-                  movie.poster_path
-                    ? `${base_url}${file_size}${movie.poster_path}`
-                    : `${process.env.PUBLIC_URL + '/no_poster.jpg'}`
-                }
-                alt={movie.title}
-              />
-              <Title>{movie.title}</Title>
-              <Info>
-                <Genres ids={movie.genre_ids} />
-                {movie.release_date && <Release>{movie.release_date}</Release>}
-              </Info>
-            </Movie>
-          );
-        })}
+      {movie && (
+        <Movie>
+          <Poster
+            src={
+              movie.poster_path
+                ? `${base_url}${file_size}${movie.poster_path}`
+                : `${process.env.PUBLIC_URL + '/no_poster.jpg'}`
+            }
+            alt={movie.title}
+          />
+          <Title>{movie.title}</Title>
+          <Info>
+            <Genres ids={movie.genre_ids} />
+            {movie.release_date && <Release>{movie.release_date}</Release>}
+          </Info>
+        </Movie>
+      )}
     </>
   );
 }
