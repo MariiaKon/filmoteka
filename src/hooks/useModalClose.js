2a267f470a99d3783d8be0948b00e65e1ed24b4e
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-11-20 22:42:03 +0200

    added close modal on overlay click

--- src/hooks/useModalClose.js
+++ src/hooks/useModalClose.js
@@ -5,8 +5,13 @@ function useModalClose(onClick) {
     const closeOnEsc = e => (e.key === 'Escape' ? onClick() : null);
     document.body.addEventListener('keydown', closeOnEsc);
 
+    const closeOnOverlayClick = e =>
+      e.target.id === 'overlay' ? onClick() : null;
+    document.body.addEventListener('click', closeOnOverlayClick);
+
     return () => {
       document.body.removeEventListener('keydown', closeOnEsc);
+      document.body.removeEventListener('click', closeOnOverlayClick);
     };
   }, [onClick]);
 }
