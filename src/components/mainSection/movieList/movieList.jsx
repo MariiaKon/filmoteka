bfa8343f506376be1d913d4ab18015622f3b6a9f
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-11-22 04:52:46 +0200

    fixed <MovieList/>

--- src/components/mainSection/movieList/movieList.jsx
+++ src/components/mainSection/movieList/movieList.jsx
@@ -1,18 +1,12 @@
-import { useOutletContext } from 'react-router-dom';
-import useGetMovies from 'hooks/useGetMovies';
 import MovieItem from 'components/mainSection/movieItem/movieItem';
 import { List } from './movieList.styled';
 
-function MovieList() {
-  const { query } = useOutletContext();
-  const movies = useGetMovies(query);
-
+function MovieList({ movies }) {
   return (
     <List>
-      {movies &&
-        movies.map(movie => {
-          return <MovieItem key={movie.id} movie={movie} />;
-        })}
+      {movies.map(movie => {
+        return <MovieItem key={movie.id} movie={movie} />;
+      })}
     </List>
   );
 }
--- src/views/homeView/homeView.jsx
+++ src/views/homeView/homeView.jsx
@@ -1,10 +1,15 @@
+import { useOutletContext } from 'react-router-dom';
+import useGetMovies from 'hooks/useGetMovies';
 import MovieList from 'components/mainSection/movieList/movieList';
 import Pages from 'components/mainSection/pagination/pagination';
 
 function HomeView() {
+  const { query } = useOutletContext();
+  const movies = useGetMovies(query);
+
   return (
     <>
-      <MovieList />
+      {movies && <MovieList movies={movies} />}
       <Pages />
     </>
   );
