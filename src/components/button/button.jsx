0bedd5aa21eb954d18d9e57372412c006c6ba2d9
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-11-24 00:18:00 +0200

    export changed to export default in <Button/>

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,9 +1,11 @@
 import { Btn } from 'components/button/button.styled';
 
-export function Button({ type = 'submit', children, onClick }) {
+function Button({ type = 'submit', children, onClick }) {
   return (
     <Btn type={type} onClick={onClick}>
       {children}
     </Btn>
   );
 }
+
+export default Button;
