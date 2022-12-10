3dc75ad5502287107fc983c5d788d789a705115a
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-12-10 20:05:57 +0200

    fixed <Logo/>

--- src/components/headerSection/logo/logo.jsx
+++ src/components/headerSection/logo/logo.jsx
@@ -1,9 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { setQuery } from 'redux/reducers/querySlice';
+import { setPage } from 'redux/reducers/pageSlice';
 import { ReactComponent as LogoSvg } from 'icons/logo.svg';
 import { LogoTitle, Navlink } from './logo.styled';
 
 function Logo() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setQuery(''));
+    dispatch(setPage(1));
+  };
+
   return (
-    <Navlink to="/">
+    <Navlink to="/" onClick={handleClick}>
       <LogoSvg />
       <LogoTitle>Filmoteka</LogoTitle>
     </Navlink>
