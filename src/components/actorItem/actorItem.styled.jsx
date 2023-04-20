b7003483e7cca5775c21ae67a790f1b51cda69cb
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2023-04-20 23:08:10 +0300

    fixed CSS layout for actors view

--- src/components/actorItem/actorItem.styled.jsx
+++ src/components/actorItem/actorItem.styled.jsx
@@ -5,12 +5,24 @@ import { Poster, Title, Info } from 'components/movieItem/movieItem.styled';
 export const Actor = styled.li`
   display: flex;
   gap: 10px;
+  height: calc((${varsCss.mobile}px - 20px * 2) * 0.4 / 2 * 3);
+
+  @media screen and (min-width: ${varsCss.tablet}px) {
+    height: calc((${varsCss.tablet}px - 30px - 75px * 2) / 2 * 0.4 / 2 * 3);
+  }
+
+  @media screen and (min-width: ${varsCss.desktop}px) {
+    height: calc(
+      (${varsCss.desktop}px - 30px * 2 - 71px * 2) / 3 * 0.4 / 2 * 3
+    );
+  }
 `;
 
 export const Photo = styled(Poster)`
   width: 40%;
-  height: auto;
+  height: 100%;
   margin-bottom: 0;
+  aspect-ratio: 2/3;
 `;
 
 export const Name = styled(Title)`
