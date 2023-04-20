c69625196677bf8724ba2aef354b5a5c5affd906
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2023-04-20 21:36:57 +0300

    fixed CSS for trailer on mobile

--- src/components/trailer/trailer.styled.jsx
+++ src/components/trailer/trailer.styled.jsx
@@ -3,12 +3,19 @@ import styled from 'styled-components';
 
 export const Video = styled.iframe`
   width: 100%;
-  aspect-ratio: 16/9;
+  aspect-ratio: 3/4;
   border-radius: 5px;
+  background: ${varsCss.secondaryColor};
+  border: 2px solid ${varsCss.textSecondary};
 
   @media screen and (min-width: ${varsCss.tablet}px) {
     position: static;
     grid-area: 1 / 1 / 2 / 3;
+    aspect-ratio: 16/9;
+  }
+
+  @media screen and (orientation: landscape) {
+    aspect-ratio: 16/9;
   }
 
   :hover {
