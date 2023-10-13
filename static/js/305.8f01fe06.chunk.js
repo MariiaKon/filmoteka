d15a95bfe7a19c92a51e66b21eaa1ecff956adb9
MariiaKon <m.konovalchuk@ukr.net>
2023-10-13 13:05:03 +0000

    Deploying to gh-pages from @ MariiaKon/filmoteka@0a837d93e3a2d76bf22a8c1a76217b6d85ddb86d 🚀

--- static/js/305.8f01fe06.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[305],{7305:function(e,s,t){t.r(s);var r=t(7689),a=t(286),n=t(4189),u=t(3329);s.default=function(){var e=(0,r.bx)(),s=e.movies,t=e.actors,l=e.error,p=e.totalResults;return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(a.Z,{items:0!==s.length?s:t}),!l&&(0,u.jsx)(n.Z,{totalResults:p,perPage:20})]})}}}]);
-//# sourceMappingURL=305.8f01fe06.chunk.js.map
